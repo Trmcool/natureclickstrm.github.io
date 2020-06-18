4039bc852614b0d9c5083a3013b529e89e7b2c11
Trmcool <tanmayrm890@gmail.com>
2020-06-19 01:43:25 +0530

    Update nature1.js

--- nature1.js
+++ nature1.js
@@ -2,22 +2,3 @@ function show(bg)
 {
     document.getElementById('hey').style.backgroundImage= "url("+bg.src+")";
 }
-
-function menu()
-{
-  var x= document.getElementById('navi').querySelectorAll('a');
-  var i;
-  for ( i = 0; i < x.length; i++) {
-      x[i].style.display="block";
-      
-  }
-}
-
-function nomenu(){
-    var z = document.getElementById('navi').querySelectorAll('a');
-    var j;
-    for (j= 0; j < z.length; j++) {
-        z[j].style.display = "none";
-
-    }
-}
