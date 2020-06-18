db45fbcf1e1202846a8ba5d67cefbf506f7ae22f
Trmcool <tanmayrm890@gmail.com>
2020-06-18 16:12:59 +0530

    Update nature1.js

--- nature1.js
+++ nature1.js
@@ -2,3 +2,10 @@ function show(bg)
 {
     document.getElementById('hey').style.backgroundImage= "url("+bg.src+")";
 }
+
+
+    function grid() {
+        for (i = 0; i < elements.length; i++) {
+            elements[i].style.msFlex = "25%";  // IE10
+            elements[i].style.flex = "25%";
+        }
