642a3da4899cabeb41c386ecd469f5904369dbe5
Trmcool <tanmayrm890@gmail.com>
2020-06-18 23:20:04 +0530

    Update nature1.js

--- nature1.js
+++ nature1.js
@@ -2,3 +2,8 @@ function show(bg)
 {
     document.getElementById('hey').style.backgroundImage= "url("+bg.src+")";
 }
+
+function menu()
+{
+   document.getElementById('navi').style.display= "visible";
+}
